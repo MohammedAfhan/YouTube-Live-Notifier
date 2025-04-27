3bf1bcab140f425be39dfaab5c234406d3b3ef17
AFhAn <error5afhan@gmail.com>
2025-04-28 02:48:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();  // To load environment variables from a .env file
+
 const express = require('express');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
@@ -5,8 +7,6 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const axios = require('axios');
-
-require('dotenv').config();  // To load environment variables from a .env file
 
 // Configuration (from environment variables)
 
